8a3cf496393cb0a9b21a8f11ace00c05367fd463
mohammad Nasruddin <nasruddin@nssit.com.my>
2023-11-11 22:09:12 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeedingPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
